824bd11b41553eeddcb9360fc3696f5e21e70d16
Maré Augustin Millogo <148946708+mareaugustin@users.noreply.github.com>
2025-05-08 09:03:13 +0000

    configurations cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://cv-generator-beta-ten.vercel.app/', 'http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
